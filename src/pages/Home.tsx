5032385688af61a05185e294f8e5edb98739825b
NexGenTeck <nexgenteckcom@gmail.com>
2026-07-25 01:11:58 +0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -97,7 +97,7 @@ export const Home: React.FC = () => {
       descKey: 'services.web.desc',
       icon: '💻',
       link: '/services/web-development',
-      image: 'public/services/web_development.jpg',
+      image: '/services/web_development.jpg',
     },
     {
       titleKey: 'services.mobile',
